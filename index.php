26929d4f59256efc9af049b7b2411415a7d38d62
vincentbarmedilla <vincentarmedilla@yahoo.com>
2020-07-09 06:49:10 +0800

    test

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 require_once('config/config.php');
-
+test2
 #echo 'Please update the $conf[\'settings\'][\'script.url\'] setting in your config file to be http://' . $_SERVER['SERVER_NAME'] . str_replace('/index.php', '', $_SERVER['PHP_SELF']) . '/Web';
 #echo '<br/>You will be redirected automatically in 20 seconds, but portions of Booked Scheduler will not function correctly.';
 
